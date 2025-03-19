802577042c9c939ca71eb7c9afaaa22c151d3a8c
Strive-Vexil <seaninator1213@gmail.com>
2025-03-19 10:30:33 -0700

    week10

---
+++ week9/index.js
@@ -0,0 +1,21 @@
+// let scoops = 5;
+// while (scoops > 0) {
+// 	document.write("Another scoop!<br>");
+// 	if (scoops < 3) {
+// 		alert("Ice Cream is running low");
+// 	}
+// 	else if (scoops >= 5) {
+// 		alert("Eat faster, the ice cream is going to melt!")
+// 	}
+// 	scoops = scoops -1;
+// }
+// document.write("Life without ice cream isn't the same");
+
+console.log(Math.floor(Math.random() * 10));
+
+console.log(Math.floor(Math.random()));
+
+console.log(Math.floor(10 + Math.random() * 10));
+
+console.log(Math.floor(10 + Math.random() * 90)); // 99
+// Math.floor(min + Math.random() * max+min-1);
